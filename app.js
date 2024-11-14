7402c7680c34b2b0be96c13655985c6fb98bfcfc
DarkhaaKhuslen <167779315+DarkhaaKhuslen@users.noreply.github.com>
2024-11-14 14:39:51 +0800

    D.Khuslen2

---
+++ app.js
@@ -0,0 +1,63 @@
+// if,else-> nuhtsol shalgah(tentsuu bnuu , ih bnuu , baga bnuu , )
+// >,<,==,!=tentsuu bish bn,>= ih biyu tenstuu, <=baga buyu tentsuu\
+var nas=18;
+if(nas>17){
+    console.log("nasand hursen");
+}else{
+    console.log("nasand hureegui");
+}
+var une=50000;
+if(une<60000){
+    console.log("untei bn");
+}else{
+    console.log("hyamd")
+}
+var too2=5, too3=10;
+if(too2<too3){
+    console.log(too2+" ene too baga bn");
+}else{
+    console.log(too3+" ene too ih bn");
+}
+var too=-5;
+if(too<0){
+    console.log(too+" bol sorog too")
+}else{
+    console.log(too+" bol eyreg too")
+}
+var sondgoi=9;
+if(sondgoi%2==0){
+    console.log("tegsh too")
+}else{
+    console.log("sodgoi too")
+}
+// && -> And/bas , || -> or/eswel
+var too5=35;
+if(too5%5==0 && too5%7==0){
+    console.log("huwaagdana");
+}else{
+    console.log("huwaagdahgui")
+}
+var too5=35;
+if(too5%5==0 || too5%7==0){
+    console.log("huwaagdana");
+}else{
+    console.log("huwaagdahgui")
+}
+var age=2;
+if(age>=1 && age<3){
+    console.log("nylh");
+}else if (age>=3 && age<5){
+    console.log("balchir");
+}else{
+
+}
+var math=50, math1=40,math2=30;
+if(math>=math1 && math>math2){
+    console.log(math+" ih");
+}else if(math1>=math1 && math1>math2){
+    console.log(mat2+" ih");
+}else{
+    console.log(mat2+" ih");
+}
+
+
